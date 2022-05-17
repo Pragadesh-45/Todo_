ddefd696a17174c1c528c29b80c3ee5e54d7dbf6
Pragadesh-45 <54320162+Pragadesh-45@users.noreply.github.com>
2022-05-17 13:13:51 +0530

    Add files via upload

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+const Header = () => {
+    return (
+        <header >
+            <div class="grid-container"><h1>To Do List</h1></div>
+            
+        </header>
+    );
+};
+
+export default Header;
